4e86fd57571d3f9bc51057ad52c21836a48545af
Hanmin Park <hanmpark@student.42nice.fr>
2023-04-14 17:13:06 +0200

    Created philo_routine.c

---
+++ philo/src/main.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/08 17:15:23 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/04/14 14:51:14 by hanmpark         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	t_table	*table;
+
+	table = NULL;
+	if (argc < 5 || argc > 6)
+		return (error_exit(ERR_ARGS_NBR, EXIT_FAILURE));
+	table = prepare_table(argc, argv);
+	if (!table)
+		return (error_exit(ERR_INIT_PHILO, EXIT_FAILURE));
+	philo_routine(table);
+	return (0);
+}
--- philo/src/philo_routine.c
+++ philo/src/philo_routine.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   philo_routine.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hanmpark <hanmpark@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 17:15:23 by hanmpark          #+#    #+#             */
-/*   Updated: 2023/04/13 15:27:12 by hanmpark         ###   ########.fr       */
+/*   Created: 2023/04/14 14:51:20 by hanmpark          #+#    #+#             */
+/*   Updated: 2023/04/14 14:54:48 by hanmpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	main(int argc, char **argv)
+void	*philo_routine(t_table *table)
 {
-	t_table	*table;
-
-	table = NULL;
-	if (argc < 5 || argc > 6)
-		return (error_exit(ERR_ARGS_NBR, EXIT_FAILURE));
-	table = prepare_table(argc, argv);
-	if (!table)
-		return (error_exit(ERR_INIT_PHILO, EXIT_FAILURE));
-	return (0);
+	
 }
